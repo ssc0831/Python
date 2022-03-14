122a81769cccee85b8c081fd496fcd513aa2be86
ssc0831 <ssc0831@naver.com>
2022-03-14 11:16:55 +0900

    220314

---
+++ class.py
@@ -0,0 +1,74 @@
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
+    # def getdata(self,매개변수들...)
+
+
+# a = FourCal()
+
+# a.setdata(4,5)
+# print(a.first)
+# print(a.second)
+# class에 객체를 넘겨줘서 받음
+
+# a = FourCal()
+# b = FourCal()
+# a.setdata(4,2)
+# b.setdata(3,7)
+# print(id(a.first))
+# print(id(b.first))
+
+# 더하기 기능 만들기
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
+    
+#     def add(self):
+#         result = self.first + self.second
+#         return result
+
+# a = FourCal()
+# a.setdata(4,2)
+# print(a.add())
+
+# 나머지 사칙연산(빼기, 곱하기, 나누기)
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
+    
+#     def sub(self):
+#         result = self.first - self.second
+#         return result
+
+# a = FourCal()
+# a.setdata(4,2)
+# print(a.sub())
+
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
+    
+#     def mul(self):
+#         result = self.first * self.second
+#         return result
+
+# a = FourCal()
+# a.setdata(4,2)
+# print(a.mul())
+
+# class FourCal:
+#     def setdata(self, first, second):
+#         self.first = first
+#         self.second = second
+    
+#     def div(self):
+#         result = self.first / self.second
+#         return result
+
+# a = FourCal()
+# a.setdata(4,2)
+# print(a.div())
---
+++ inheritance.py
@@ -0,0 +1,43 @@
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+
+
+# add(), mul(), sub(), div() -> FourCal Class
+# class MoreFourCal(FourCal):
+#     def pow(self):
+#         result = self.first ** self.second
+#         return result
+
+# a = MoreFourCal(4,2)
+# print(a.pow())
+# print(a.add())
+# print(a.sub())
+# print(a.mul())
+# print(a.div())
+
+# Method Overiding
+# class SafeFourCal(FourCal):
+#     def div(self):
+#         if self.second == 0:
+#             return  0
+#         else:
+#             return self.first / self.second
+# a = SafeFourCal(4,0)
+# print(a.div())
