1f3299a6a2fe7ff95a4b10c661aee1272a2b660b
ssc0831 <ssc0831@naver.com>
2022-03-14 16:00:19 +0900

    20220314

---
+++ try1.py
@@ -0,0 +1,17 @@
+# c = 0
+# a = [1,2,3]
+# try:
+#    c = 10 / 2
+#    print(a[1])
+
+# 0으로 나누기 오류
+# except ZeroDivisionError:
+#     print("0으로 나누기 했습니다.")
+
+# 인덱스 오류
+# except IndexError:
+#     print(e)
+# else:
+#     print("오류가 있던없던 실행됨.")
+
+# print("항상 실행됨")
